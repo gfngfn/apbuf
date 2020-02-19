3223f2c0c2d4d8503d4b14ead42776454eb5d6d0
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-02-20 04:12:02 +0900

    dev 'cmdliner'

--- src/main.ml
+++ src/main.ml
@@ -2,9 +2,13 @@
 open Cmdliner
 
 
-let print () =
-  print_endline "Hello, world!"
+let print s =
+  print_endline ("Hello, world! " ^ s)
+
+
+let arg_in =
+  Arg.(required & pos 0 (some file) None (info []))
 
 
 let () =
-  Term.(exit (eval (const print $ const (), info "hello-world")))
+  Term.(exit (eval (const print $ arg_in, info "hello-world")))
