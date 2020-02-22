7192cd12868fe4cc17ca12e0da8ef24836613780
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-02-22 16:12:32 +0900

    migrate some functions from 'main.ml' to 'types.ml'

--- src/main.ml
+++ src/main.ml
@@ -3,51 +3,6 @@ open Cmdliner
 open Types
 
 
-let rec normalize_message (pmsg : parsed_message) : (message, error) result =
-  let open ResultMonad in
-  match pmsg with
-  | PName(name) ->
-      return (Name(name))
-
-  | PRecord(prcd) ->
-      prcd |> List.fold_left (fun res (key, pmsgsub) ->
-        res >>= fun rcdmap ->
-        if rcdmap |> RecordMap.mem key then
-          error (FieldDefinedMoreThanOnce{ key = key; })
-        else
-          normalize_message pmsgsub >>= fun msgsub ->
-          return (rcdmap |> RecordMap.add key msgsub)
-      ) (return RecordMap.empty) >>= fun rcdmap ->
-      return (Record(rcdmap))
-
-  | PVariant(pvariant) ->
-      pvariant |> List.fold_left (fun res (ctor, pmsgsubopt) ->
-        res >>= fun variantmap ->
-        if variantmap |> VariantMap.mem ctor then
-          error (ConstructorDefinedMoreThanOnce{ constructor = ctor; })
-        else
-          begin
-            match pmsgsubopt with
-            | None          -> return None
-            | Some(pmsgsub) -> normalize_message pmsgsub >>= fun msgsub -> return (Some(msgsub))
-          end >>= fun msgsubopt ->
-          return (variantmap |> VariantMap.add ctor msgsubopt)
-      ) (return VariantMap.empty) >>= fun variantmap ->
-      return (Variant(variantmap))
-
-
-let normalize_declarations (pdecls : parsed_declarations) : (declarations, error) result =
-  let open ResultMonad in
-  pdecls |> List.fold_left (fun res (name, pmsg) ->
-    res >>= fun declmap ->
-    if declmap |> DeclMap.mem name then
-      error (MessageNameDefinedMoreThanOnce{ name = name; })
-    else
-      normalize_message pmsg >>= fun msg ->
-      return (declmap |> DeclMap.add name msg)
-  ) (return DeclMap.empty)
-
-
 (** This is the core part of the program.
     The parameter [s] stands for an input file path already confirmed to be existent. *)
 let main s =
@@ -68,6 +23,7 @@ let main s =
 (** The spec for the anonimous argument that points to an input. *)
 let arg_in =
   Arg.(required (pos 0 (some file) None (info [])))
+    (* `Arg.file` asserts that the given path is existent. *)
 
 
 let () =
--- src/types.ml
+++ src/types.ml
@@ -72,3 +72,48 @@ end = struct
     Ok(v)
 
 end
+
+
+let rec normalize_message (pmsg : parsed_message) : (message, error) result =
+  let open ResultMonad in
+  match pmsg with
+  | PName(name) ->
+      return (Name(name))
+
+  | PRecord(prcd) ->
+      prcd |> List.fold_left (fun res (key, pmsgsub) ->
+        res >>= fun rcdmap ->
+        if rcdmap |> RecordMap.mem key then
+          error (FieldDefinedMoreThanOnce{ key = key; })
+        else
+          normalize_message pmsgsub >>= fun msgsub ->
+          return (rcdmap |> RecordMap.add key msgsub)
+      ) (return RecordMap.empty) >>= fun rcdmap ->
+      return (Record(rcdmap))
+
+  | PVariant(pvariant) ->
+      pvariant |> List.fold_left (fun res (ctor, pmsgsubopt) ->
+        res >>= fun variantmap ->
+        if variantmap |> VariantMap.mem ctor then
+          error (ConstructorDefinedMoreThanOnce{ constructor = ctor; })
+        else
+          begin
+            match pmsgsubopt with
+            | None          -> return None
+            | Some(pmsgsub) -> normalize_message pmsgsub >>= fun msgsub -> return (Some(msgsub))
+          end >>= fun msgsubopt ->
+          return (variantmap |> VariantMap.add ctor msgsubopt)
+      ) (return VariantMap.empty) >>= fun variantmap ->
+      return (Variant(variantmap))
+
+
+let normalize_declarations (pdecls : parsed_declarations) : (declarations, error) result =
+  let open ResultMonad in
+  pdecls |> List.fold_left (fun res (name, pmsg) ->
+    res >>= fun declmap ->
+    if declmap |> DeclMap.mem name then
+      error (MessageNameDefinedMoreThanOnce{ name = name; })
+    else
+      normalize_message pmsg >>= fun msg ->
+      return (declmap |> DeclMap.add name msg)
+  ) (return DeclMap.empty)
