d0a6f8176620426bcb39742fdb986c4a07c3e1d5
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-03-10 22:20:47 +0900

    begin to develop 'GenScala' (midway 2)

--- src/genScala.ml
+++ src/genScala.ml
@@ -5,11 +5,16 @@ open Types
 module Output : sig
   type identifier
   type tree
+  type type_identifier
+  type type_parameter
+  type typ
+  type declaration
   val local_for_parameter : Types.variable -> identifier
   val global_reads : Name.t -> identifier
   val identifier : identifier -> tree
   val application : tree -> tree list -> tree
   val make_record_reads : (Key.t * tree) list -> tree
+  val stringify_declaration : declaration -> string
 end = struct
 end
 
@@ -34,5 +39,25 @@ let rec generate_message_decoder (msg : message) : Output.tree =
       Output.make_record_reads entries
 
 
-let generate (decls : declarations) =
-  ()
+let generate (package_name : string) (decls : declarations) =
+  let odecls : Output.declaration list =
+    DeclMap.fold (fun _name def acc ->
+      match def.def_main with
+      | BuiltIn(_builtin)      -> acc (* TODO *)
+      | GivenNormal(_msg)      -> acc (* TODO *)
+      | GivenVariant(_variant) -> acc (* TODO *)
+    ) decls Alist.empty |> Alist.to_list
+  in
+  let sdecls =
+    odecls |> List.map (fun odecl ->
+      Output.stringify_declaration odecl ^ "\n\n"
+    )
+  in
+  List.append [
+    Format.sprintf "package %s\n" package_name;
+    "\n";
+    "import play.api.libs.json._\n";
+    "import play.api.libs.json.Reads._\n";
+    "import play.api.libs.functional.syntax._\n";
+    "\n";
+  ] sdecls |> String.concat ""
